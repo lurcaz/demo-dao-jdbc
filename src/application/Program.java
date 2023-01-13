23b81bbb7f168129b7390fd6831d4436c338a581
Lucas <lucas.desa@outlook.com.br>
2023-01-13 13:17:33 -0300

    Reuse instatiations

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,17 @@
 package application;
 
-
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-
+		
+	
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("==== TEST 1 : Seller FindById ====");		
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
